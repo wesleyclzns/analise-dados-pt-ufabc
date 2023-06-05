77d6d6851ce6d0ce932d16d489b3a22dd68187f6
Wesley Café Calazans <wesclzns@gmail.com>
2023-06-05 11:59:05 -0300

    aula 2

---
+++ analise-dados/aula2/aula2.R
@@ -0,0 +1,11 @@
+#Aula 2
+# setwd("C:/Users/w.cafe/Documents/analise-dados")
+
+#IMPORTAÇÃO DA TABELA
+# agua1 <- read.csv2("~/analise-dados/aula2/dados/agua1.csv")
+
+names(agua1)
+head(agua1)
+tail(agua1)
+str(agua1)
+summary(agua1)
